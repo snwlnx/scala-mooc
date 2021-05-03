5a9f5c567bd1d41b496b60818f7372a8676f6335
nellashapiro <nella_shapiro@apple.com>
2021-05-03 16:02:41 -0700

    Create solution for HW2

--- src/main/scala/homeworks/collections/task_caesar.scala
+++ src/main/scala/homeworks/collections/task_caesar.scala
@@ -4,28 +4,58 @@ import homeworks.HomeworksUtils.TaskSyntax
 
 object task_caesar {
 
-  /**
-   * В данном задании Вам предлагается реализовать функции,
-   * реализующие кодирование/декодирование строки шифром Цезаря.
-   * https://ru.wikipedia.org/wiki/Шифр_Цезаря
-   * Алфавит - прописные латинские буквы от A до Z.
-   * Сдвиг   - неотрицательное целое число.
-   * Пример: при сдвиге 2 слово "SCALA" шифруется как "UECNC".
-   */
-  /**
-   * @param word   входное слово, которое необходимо зашифровать
-   * @param offset сдвиг вперёд по алфавиту
-   * @return зашифрованное слово
-   */
-  def encrypt(word: String, offset: Int): String =
-    task"Реализуйте метод `encrypt`"()
+  val letters = ('A' to 'Z').toVector
+
+  val lettersMap = letters.zipWithIndex.toMap
 
   /**
-   * @param cipher шифр, который необходимо расшифровать
-   * @param offset сдвиг вперёд по алфавиту
-   * @return расшифрованное слово
-   */
-  def decrypt(cipher: String, offset: Int): String =
-    task"Реализуйте метод `decrypt`"()
+    * В данном задании Вам предлагается реализовать функции,
+    * реализующие кодирование/декодирование строки шифром Цезаря.
+    * https://ru.wikipedia.org/wiki/Шифр_Цезаря
+    * Алфавит - прописные латинские буквы от A до Z.
+    * Сдвиг   - неотрицательное целое число.
+    * Пример: при сдвиге 2 слово "SCALA" шифруется как "UECNC".
+    */
+  /**
+    * @param word   входное слово, которое необходимо зашифровать
+    * @param offset сдвиг вперёд по алфавиту
+    * @return зашифрованное слово
+    */
+  def encrypt(word: String, offset: Int): String = {
+    val newOffset = offset % 26
 
+    word.toSeq
+      .map(letter => newIndex(lettersMap(letter), newOffset, letters.size))
+      .map(letters(_))
+      .mkString("")
+  }
+  task"Реализуйте метод `encrypt`" ()
+
+  /**
+    * @param cipher шифр, который необходимо расшифровать
+    * @param offset сдвиг вперёд по алфавиту
+    * @return расшифрованное слово
+    */
+  def decrypt(cipher: String, offset: Int): String = {
+    val newOffset = offset % 26
+
+    cipher.toSeq
+      .map(
+        letter => newDecryptedIndex(lettersMap(letter), newOffset, letters.size)
+      )
+      .map(letters(_))
+      .mkString("")
+
+  }
+  task"Реализуйте метод `decrypt`" ()
+
+  private def newIndex(index: Int, offset: Int, size: Int): Int = {
+    val newIndex = index + offset
+    if (newIndex >= size) newIndex - size else newIndex
+  }
+
+  private def newDecryptedIndex(index: Int, offset: Int, size: Int) = {
+    val newIndex = index - offset
+    if (newIndex < 0) size + newIndex else newIndex
+  }
 }
--- src/main/scala/homeworks/collections/task_seq_riddle.scala
+++ src/main/scala/homeworks/collections/task_seq_riddle.scala
@@ -5,30 +5,79 @@ import homeworks.HomeworksUtils.TaskSyntax
 object task_seq_riddle {
 
   /**
-   * Рассмотрим последовательность с числами:
-   *
-   * 1
-   * 1 1
-   * 2 1
-   * 1 2 1 1
-   * 1 1 1 2 2 1
-   * 3 1 2 2 1 1
-   * ...........
-   *
-   * 1. Реализуйте функцию генерирующую след последовательность из текущей
-   * */
+    * Рассмотрим последовательность с числами:
+    *
+    * 1
+    * 1 1
+    * 2 1
+    * 1 2 1 1
+    * 1 1 1 2 2 1
+    * 3 1 2 2 1 1
+    * ...........
+    *
+    * 1. Реализуйте функцию генерирующую след последовательность из текущей
+    * */
+  def nextLine(line: Seq[Int]): List[Int] = {
 
-  def nextLine(currentLine: List[Int]): List[Int] =
-    task"Реализуйте функцию генерирующую след последовательность из текущей"()
+    def acc(line: Seq[Int], counter: Int, prev: Int): Seq[Int] = {
+      val newList = line match {
+        case Nil => List(1, prev)
+        case head :: Nil => {
+          if (prev == head)
+            Seq(counter + 1, head)
+          else
+            Seq(counter, prev, 1, head)
+        }
+        case el2 :: xs => {
+          if (prev == el2) {
+            acc(xs.asInstanceOf[Seq[Int]], counter + 1, el2)
+          } else {
+            Seq(counter, prev.asInstanceOf[Int]) ++ acc(
+              xs.asInstanceOf[Seq[Int]],
+              1,
+              el2
+            )
+          }
+        }
+
+      }
+
+      newList.asInstanceOf[Seq[Int]]
+    }
+
+    acc(line.tail, 1, line.head).toList
+  }
+
+  case class test(res: List[Int], counter: Int, current: Int, i: Int)
+  def nextLine1(line: Seq[Int]): List[Int] = {
+    val result = line.foldRight(test(List[Int](), 0, 0, line.size - 1)) {
+      (x, z) =>
+        if ((z.current == x || z.counter == 0) && z.i > 0) {
+          test(z.res, z.counter + 1, x, z.i - 1)
+        } else {
+          z.i match {
+            case 0 if z.current == x =>
+              test(List(z.counter + 1, z.current) ::: z.res, 1, x, z.i - 1)
+            case 0 if z.current != x =>
+              test(List(1, x, z.counter, z.current) ::: z.res, 1, x, z.i - 1)
+            case _ => test(List(z.counter, z.current) ::: z.res, 1, x, z.i - 1)
+          }
+        }
+    }
+
+    result.res
+  }
+  task"Реализуйте функцию генерирующую след последовательность из текущей" ()
 
   /**
-   * 2. Реализуйте ленивый список, который генерирует данную последовательность
-   * Hint: см. LazyList.cons
-   *
-   * lazy val funSeq: LazyList[List[Int]]  ...
-   *
-   */
-
-  val funSeq: LazyList[List[Int]] =
-    task"Реализуйте ленивый список, который генерирует данную последовательность"()
+    * 2. Реализуйте ленивый список, который генерирует данную последовательность
+    * Hint: см. LazyList.cons
+    *
+    * lazy val funSeq: LazyList[List[Int]]  ...
+    *
+    */
+  val funSeq: LazyList[List[Int]] = List(1) #:: funSeq.map(
+    line => nextLine(line)
+  )
+  task"Реализуйте ленивый список, который генерирует данную последовательность" ()
 }
