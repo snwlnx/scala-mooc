50a746502751ee4277810ed35c09de91290c6be8
Красняк Илья Сергеевич <krasnyak@tochka.com>
2021-09-11 15:21:29 +0500

    done

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -1,25 +1,36 @@
 package homeworks.futures
 
-import homeworks.HomeworksUtils.TaskSyntax
-
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object task_futures_sequence {
 
   /**
-   * В данном задании Вам предлагается реализовать функцию fullSequence,
-   * похожую на Future.sequence, но в отличии от нее,
-   * возвращающую все успешные и не успешные результаты.
-   * Возвращаемое тип функции - кортеж из двух списков,
-   * в левом хранятся результаты успешных выполнений,
-   * в правово результаты неуспешных выполнений.
-   * Не допускается использование методов объекта Await и мутабельных переменных var
-   */
+    * В данном задании Вам предлагается реализовать функцию fullSequence,
+    * похожую на Future.sequence, но в отличии от нее,
+    * возвращающую все успешные и не успешные результаты.
+    * Возвращаемое тип функции - кортеж из двух списков,
+    * в левом хранятся результаты успешных выполнений,
+    * в правово результаты неуспешных выполнений.
+    * Не допускается использование методов объекта Await и мутабельных переменных var
+    */
   /**
-   * @param futures список асинхронных задач
-   * @return асинхронную задачу с кортежом из двух списков
-   */
-  def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+    * @param futures список асинхронных задач
+    * @return асинхронную задачу с кортежом из двух списков
+    */
+  def fullSequence[A](
+      futures: List[Future[A]]
+  )(implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    Future.sequence(
+      futures
+        .map(_.transform(Success(_)))
+    ) map (listTry =>
+      listTry.foldLeft(Tuple2(List[A](), List[Throwable]()))((acc, tryResult) =>
+        (acc, tryResult) match {
+          case (_, Success(v))  => (acc._1 :+ v, acc._2)
+          case (_, Failure(ex)) => (acc._1, acc._2 :+ ex)
+        }
+      )
+    )
+  }
 }
